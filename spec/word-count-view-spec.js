e1c34df0b36c0d8ab0cb74e192e0833dfdb42608
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-30 13:51:59 -0300

    removing tests

--- spec/word-count-view-spec.js
+++ spec/word-count-view-spec.js
@@ -2,8 +2,8 @@ var WordCountView;
 
 WordCountView = require('../lib/word-count-view');
 
-describe("WordCountView", function() {
-  it("has one valid test", function() {
-    expect("life").toBe("easy");
-  });
-});
+// describe("WordCountView", function() {
+//   it("has one valid test", function() {
+//     expect("life").toBe("easy");
+//   });
+// });
